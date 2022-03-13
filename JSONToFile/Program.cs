23e1ddc8a63f03625fb21863ffced4e730674271
GreedNeSS <ruslan199126@gmail.com>
2022-03-13 21:19:02 +0300

    Добавьте файлы проекта.

---
+++ JSONToFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+
+namespace JSONToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** JSON To File *****");
+
+            string path = @"F:\Text\Person.json";
+
+            using FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
+        }
+    }
+}
---
+++ SimpleSerializeDeserialize/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+
+namespace SimpleSerializeDeserialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Simple Serialize Deserialize JSON *****");
+
+            Person person = new Person("Ruslan", 30);
+            string perStr = JsonSerializer.Serialize<Person>(person);
+
+            Console.WriteLine($"Json: {perStr}");
+
+            Person? restoredPerson = JsonSerializer.Deserialize<Person>(perStr);
+
+            Console.WriteLine($"restoredPerson.Name: {restoredPerson?.Name}");
+        }
+    }
+}
