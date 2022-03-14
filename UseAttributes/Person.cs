a62ff6656fae4d27c1f9b4ce006ddc37ccd65c38
GreedNeSS <ruslan199126@gmail.com>
2022-03-14 17:29:12 +0300

    Use attributes

---
+++ UseAttributes/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace UseAttributes
+{
+    class Person
+    {
+        [JsonPropertyName("firstname")]
+        public string Name { get; set; }
+        [JsonIgnore]
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
--- UseAttributes/Program.cs
+++ UseAttributes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Json;
 
 namespace UseAttributes
 {
@@ -6,7 +7,22 @@ namespace UseAttributes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Use Attributes *****");
+
+            Person person = new Person()
+            {
+                Name = "Ruslan",
+                Age = 30
+            };
+
+            string json = JsonSerializer.Serialize<Person>(person);
+
+            Console.WriteLine($"\nperson: {person}");
+            Console.WriteLine($"JSON: {json}");
+
+            Person? restoredPerson = JsonSerializer.Deserialize<Person>(json);
+
+            Console.WriteLine($"restoredPerson: {restoredPerson}");
         }
     }
 }
